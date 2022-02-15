b05a7fea121a73c373bb2011f6aaea043b4d600f
Victor Nevola <victornevolapn@gmail.com>
2022-02-15 00:43:19 -0300

    feat: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="icon" href="https://www.marvel.com/static/images/favicon/favicon.ico" type="image/x-icon" sizes="16x16 32x32" />
+          <link rel="icon" href="https://www.marvel.com/static/images/favicon/favicon-16x16.png" type="image/png" sizes="16x16" />
+          <link rel="icon" href="https://www.marvel.com/static/images/favicon/favicon-32x32.png" type="image/png" sizes="32x32" />
+          <link rel="icon" href="https://www.marvel.com/static/images/favicon/apple-touch-icon-57.png" type="image/png" sizes="57x57" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet" />
