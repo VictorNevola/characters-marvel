8c4414336861ca4a9554bf635c3acc4ef46ec937
Victor Nevola <victornevolapn@gmail.com>
2022-02-17 00:04:59 -0300

    test: create test to verify banner has render and size image

--- cypress/integration/2 - pages/home.spec.ts
+++ cypress/integration/2 - pages/home.spec.ts
@@ -1,3 +1,7 @@
 describe("Home", () => {
-  
+  it("should banner render and verify image size", () => {
+    cy.visit("/");
+    cy.get("section[data-cy='banner-container']").should("exist");
+    cy.get("section[data-cy='banner-container'] .image").invoke('width').should("be.lessThan", 1920);
+  });
 });
