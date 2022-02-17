c4fb1102f945684aa56f0df5732aa0fe77586219
Victor Nevola <victornevolapn@gmail.com>
2022-02-17 00:09:38 -0300

    feat: add banner on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/components/Banner'
 import Header from '@/components/Header'
 import type { NextPage } from 'next'
 
@@ -5,6 +6,12 @@ const Home: NextPage = () => {
   return (
     <>
      <Header />
+     <Banner 
+        urlImage="/images/banner-marvel.png"
+        imgWidth={1440}
+        imgHeight={300}
+        nameImage="Marvel Banner"
+     />
     </>
   )
 }
