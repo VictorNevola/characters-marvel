8e97ca4833e30b6ee964d394eeec0578afd488a2
Victor Nevola <victornevolapn@gmail.com>
2022-02-18 00:22:08 -0300

    remove: unused interface

--- store/search.ts
+++ store/search.ts
@@ -3,10 +3,6 @@ import { atom, selector } from "recoil";
 import fetch from "@/config/api";
 
 export const defaultValuesQuantity = [20, 40, 60, 80, 100];
-export interface SearchState {
-  searchTerm: string;
-  listCharacters: any[];
-};
 
 export enum orderByEnum {
   "asc" = "name",
