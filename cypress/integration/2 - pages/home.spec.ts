032f33ad6b2e12ab531b61a5b98d0530927126e6
Victor Nevola <victornevolapn@gmail.com>
2022-02-18 00:25:08 -0300

    test: remove old test

--- cypress/integration/2 - pages/home.spec.ts
+++ cypress/integration/2 - pages/home.spec.ts
@@ -1,7 +1,3 @@
 describe("Home", () => {
-  it("should banner render and verify image size", () => {
-    cy.visit("/");
-    cy.get("section[data-cy='banner-container']").should("exist");
-    cy.get("section[data-cy='banner-container'] .image").invoke('width').should("be.lessThan", 1920);
-  });
+  
 });
