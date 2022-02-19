a70cfa0d6aaaec00a4f958717d6ad69fd5fd2c0f
Victor Nevola <victornevolapn@gmail.com>
2022-02-19 19:47:16 -0300

    refactor: adjusting store

--- store/search.ts
+++ store/search.ts
@@ -4,7 +4,6 @@ import fetch from "@/config/api";
 import { ApiMarvel } from "interfaces/apiMarvel";
 
 export const defaultValuesQuantity = [20, 40, 60, 80, 100];
-
 export interface ParamsOptions {
   orderBy: string;
   limit: number;
@@ -16,28 +15,14 @@ export enum orderByEnum {
   "desc" =  "-name",
 };
 
+export const defaultValuesToSearch = {
+  orderBy: orderByEnum.asc,
+  limit: defaultValuesQuantity[0],
+}
+
 export const searchOptions = atom<ParamsOptions>({
   key: "searchOptions",
   default: {
-    orderBy: orderByEnum.asc,
-    limit: defaultValuesQuantity[0]
+    ...defaultValuesToSearch,
   },
-});
-
-export const customSearchCharacters = selector({
-  key: "customSearchCharacters",
-  get: async ({ get }) => {
-    const options = get(searchOptions);
-
-    const params: ParamsOptions = {
-      ...options
-    };
-
-    if(options.nameStartsWith === "") delete params.nameStartsWith;
-
-    const response = await fetch<ApiMarvel>("/characters", "GET", params);
-
-    return response.data;
-
-  }
 });
