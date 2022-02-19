87d43ac79a9fc8f908b1b6cb212165cd1429a1a6
Victor Nevola <victornevolapn@gmail.com>
2022-02-19 15:58:07 -0300

    feat: add missing params on the fetch

--- config/api.ts
+++ config/api.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosResponse, Method } from "axios";
 import { setupCache } from "axios-cache-adapter";
+import md5 from "md5";
 
 const publicKeyApi = process.env.NEXT_PUBLIC_API_KEY_MARVEL;
+const privateKeyApi = process.env.NEXT_PUBLIC_API_KEY_MARVEL_PRIVATE;
+const timeStamp = new Date().getTime();
 
-if(!publicKeyApi) {
-  throw new Error('Public Key API Marvel not found');
+if (!publicKeyApi || !privateKeyApi) {
+  throw new Error("Keys API Marvel not found");
 }
 
 type ResponseFetch<T> = {
@@ -21,6 +24,26 @@ const api = axios.create({
   baseURL: "https://gateway.marvel.com:443/v1/public",
 });
 
+api.interceptors.request.use(function (config) {
+  return config;
+});
+
+api.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.error("error api", error);
+    return Promise.reject(error);
+  }
+);
+
+const defaultParams = {
+  ts: timeStamp,
+  hash: md5(`${timeStamp + privateKeyApi + publicKeyApi}`),
+  apikey: publicKeyApi,
+};
+
 async function fetch<T>(
   url: string,
   method: Method,
@@ -34,8 +57,8 @@ async function fetch<T>(
     headers,
     data: body,
     params: {
+      ...defaultParams,
       ...params,
-      apikey: publicKeyApi,
     },
   });
 
