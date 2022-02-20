a85fc67963fc0120c033792b78201d18c1ca6701
Victor Nevola <victornevolapn@gmail.com>
2022-02-20 20:34:28 -0300

    styles: change loader for utils

--- styles/pages/home.ts
+++ styles/pages/home.ts
@@ -11,15 +11,6 @@ const shimmer = keyframes`
   }
 `;
 
-export const ldsRing = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
 export const HomeWrapper = styled(Container)`
   padding: 2.25rem 1rem;
   min-height: 100vh;
@@ -126,39 +117,4 @@ export const HomeListContent = styled.li`
   display: block;
   width: 100%;
   height: 100%;
-`;
-
-export const HomeLoader = styled.div`
-  display: block;
-  position: relative;
-  width: 80px;
-  height: 80px;
-  margin: 0 auto;
-
-  div {
-    box-sizing: border-box;
-    display: block;
-    position: absolute;
-    width: 64px;
-    height: 64px;
-    margin: 8px;
-    border: 8px solid ${(pros) => pros.theme.colors.text};
-    border-radius: 50%;
-    animation: ${ldsRing} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-    border-color: ${(pros) => pros.theme.colors.text} transparent transparent
-      transparent;
-    margin: 0 auto;
-
-    &:nth-child(1) {
-      animation-delay: -0.45s;
-    }
-
-    &:nth-child(2) {
-      animation-delay: -0.3s;
-    }
-
-    &:nth-child(3) {
-      animation-delay: -0.15s;
-    }
-  }
 `;
--- styles/utils.ts
+++ styles/utils.ts
@@ -1,8 +1,52 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+export const ldsRing = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 export const Container = styled.div`
   max-width: 1440px;
   width: auto;
   padding: 0 1rem;
   margin: 0 auto;
+`;
+
+export const SpinnerLoader = styled.div`
+  display: block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin: 0 auto;
+
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid ${(pros) => pros.theme.colors.text};
+    border-radius: 50%;
+    animation: ${ldsRing} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: ${(pros) => pros.theme.colors.text} transparent transparent
+      transparent;
+    margin: 0 auto;
+
+    &:nth-child(1) {
+      animation-delay: -0.45s;
+    }
+
+    &:nth-child(2) {
+      animation-delay: -0.3s;
+    }
+
+    &:nth-child(3) {
+      animation-delay: -0.15s;
+    }
+  }
 `;
