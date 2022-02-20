e80e1be5578e553fa1cc4bf2d1107046ec62eced
Victor Nevola <victornevolapn@gmail.com>
2022-02-20 20:11:11 -0300

    feat: created link to home in logo

--- components/Header/logo/index.tsx
+++ components/Header/logo/index.tsx
@@ -1,15 +1,22 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
+
+import { LogoLink } from "./styles";
 
 const Logo = () => {
   return (
-    <Image 
-      src="/images/logo-marvel.png"
-      alt="Logo"
-      width={98}
-      height={38}
-      data-cy="logo"
-    />
-  )
+    <Link href="/" passHref>
+      <LogoLink>
+        <Image
+          src="/images/logo-marvel.png"
+          alt="Logo"
+          width={98}
+          height={38}
+          data-cy="logo"
+        />
+      </LogoLink>
+    </Link>
+  );
 };
 
 export default Logo;
